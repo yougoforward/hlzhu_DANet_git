a7b6b4069ecb074e363ed3d32b878d80d435da4b
long <908865817@qq.com>
2018-12-06 14:50:25 +0800

    hlzhu_DANet first commit

---
+++ encoding/models/__init__.py
@@ -0,0 +1,25 @@
+from .model_zoo import get_model
+from .model_store import get_model_file
+from .base import *
+from .fcn import *
+from .psp import *
+from .encnet import *
+from .danet import *
+from .msdanet import *
+from .msdanet2 import *
+from .mview_danet import *
+from .mview_danet2 import *
+
+def get_segmentation_model(name, **kwargs):
+    from .fcn import get_fcn
+    models = {
+        'fcn': get_fcn,
+        'psp': get_psp,
+        'encnet': get_encnet,
+        'danet': get_danet,
+        'msdanet': get_msdanet,
+        'msdanet2': get_msdanet2,
+        'mvdanet': get_mvdanet,
+        'mvdanet2': get_mvdanet2,
+    }
+    return models[name.lower()](**kwargs)
