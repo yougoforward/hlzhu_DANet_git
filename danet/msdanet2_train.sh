634767d84a75e94f997614f58dcff776109a2295
long <908865817@qq.com>
2018-12-07 16:56:48 +0800

    sh

---
+++ danet/msdanet2_train.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+CUDA_VISIBLE_DEVICES=6,7 python train.py --dataset cityscapes --model  msdanet2 --backbone resnet101 --checkname msdanet2101  --base-size 1024 --crop-size 768 --epochs 240 --batch-size 8 --lr 0.003 --workers 2 --multi-grid --multi-dilation 4 8 16
