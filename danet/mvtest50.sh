fc9e49865ade61c51bd16252a6120a188971b329
long <908865817@qq.com>
2018-12-18 12:02:53 +0800

    test

--- danet/mvtest50.sh
+++ danet/mvtest50.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=6,7 python test.py --dataset cityscapes --model mvdanet --resume-dir cityscapes/mvdanet_model/mvdanet50 --base-size 2048 --crop-size 768 --workers 1 --backbone resnet50 --multi-grid --multi-dilation 4 8 16 --eval
+CUDA_VISIBLE_DEVICES=2,3 python test.py --dataset cityscapes --model mvdanet --resume-dir cityscapes/mvdanet_model/mvdanet50 --base-size 2048 --crop-size 768 --workers 1 --backbone resnet50 --multi-grid --multi-dilation 4 8 16 --eval
