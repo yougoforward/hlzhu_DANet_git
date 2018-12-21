6583380de4b0bd5a85c9d72682da56debca72c04
long <908865817@qq.com>
2018-12-21 20:14:28 +0800

    vis

--- danet/mvvis50.sh
+++ danet/mvvis50.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=2,3 python mvdanet_test.py --dataset cityscapes --model mvdanet --resume-dir cityscapes/mvdanet_model/mvdanet50 --base-size 2048 --crop-size 768 --workers 1 --backbone resnet50 --multi-grid --multi-dilation 4 8 16
+CUDA_VISIBLE_DEVICES=4,5 python mvdanet_test.py --dataset cityscapes --model mvdanet --resume-dir cityscapes/mvdanet_model/mvdanet50 --base-size 2048 --crop-size 768 --workers 1 --backbone resnet50 --multi-grid --multi-dilation 4 8 16
