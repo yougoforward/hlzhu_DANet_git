c7c8c44a7a26abe31a34d7920b6aafdd18f6a640
long <908865817@qq.com>
2018-12-23 18:53:57 +0800

    print

--- danet/mvtrain50.sh
+++ danet/mvtrain50.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=4,5 python mvdanet_train.py --mviews 49 96 --dataset cityscapes --model  mvdanet --backbone resnet50 --checkname mvdanet50_4996  --base-size 1024 --crop-size 768 --epochs 240 --batch-size 8 --lr 0.003 --workers 2 --multi-grid --multi-dilation 4 8 16
+python mvdanet_train.py --mviews 49 96 --dataset cityscapes --model  mvdanet --backbone resnet50 --checkname mvdanet50_4996  --base-size 1024 --crop-size 768 --epochs 240 --batch-size 8 --lr 0.003 --workers 2 --multi-grid --multi-dilation 4 8 16
