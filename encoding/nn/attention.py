3c084fbb0ada47ad73ab95dfb1c02c32bffe7d23
Hailong Zhu <908865817@qq.com>
2019-02-05 09:32:39 +0400

    glcnet_fast

--- encoding/nn/attention.py
+++ encoding/nn/attention.py
@@ -668,7 +668,7 @@ class reduce_PAM_Module(Module):
         self.stride = stride
 
         self.query_conv = Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
-        self.key_conv = Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=3,stride=stride)
+        self.key_conv = Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=3,stride=stride,padding=1)
         self.value_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
         self.gamma = Parameter(torch.zeros(1))
 
