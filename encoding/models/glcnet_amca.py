68d4fc4951f56f3f425154f7fb35c48fa5bc05d6
Hailong Zhu <908865817@qq.com>
2019-03-15 21:26:54 +0400

    nllnet

--- encoding/models/glcnet_amca.py
+++ encoding/models/glcnet_amca.py
@@ -92,7 +92,7 @@ class GLCNet5_amcaHead(nn.Module):
         self.conv5s = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 1, padding=0, bias=False),
                                     norm_layer(inter_channels),
                                     nn.ReLU())
-        self.aspp = selective_aggregation_ASPP_Module2(in_channels, inner_features=256, out_features=256,
+        self.aspp = selective_aggregation_ASPP_Module2(inter_channels, inner_features=256, out_features=256,
                                                       dilations=(12, 24, 36))
         self.conv52 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=1, bias=False),
                                     norm_layer(inter_channels),
