1d55160bdf87d4f6536f7ff0755dabc9ba9943b2
Hailong Zhu <908865817@qq.com>
2019-03-19 15:50:52 +0400

    amca

--- encoding/models/danet.py
+++ encoding/models/danet.py
@@ -67,7 +67,7 @@ class DANetHead(nn.Module):
                                    nn.ReLU())
 
         # self.sa = PAM_Module(inter_channels,64,512)
-        self.sa = topk_PAM_Module(inter_channels, 64, inter_channels, 4)
+        self.sa = topk_PAM_Module(inter_channels, 64, inter_channels, 10)
         self.sc = CAM_Module(inter_channels)
         self.conv51 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=1, bias=False),
                                    norm_layer(inter_channels),
