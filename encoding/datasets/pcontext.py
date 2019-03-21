6c1669943b44e63b9035a17902134b1f54c5a650
Hailong Zhu <908865817@qq.com>
2019-03-21 11:53:03 +0400

    cocostuff, pcontext

--- encoding/datasets/pcontext.py
+++ encoding/datasets/pcontext.py
@@ -16,7 +16,7 @@ from .base import BaseDataset
 
 class ContextSegmentation(BaseDataset):
     BASE_DIR = 'pcontext/VOCdevkit/VOC2010'
-    NUM_CLASS = 59
+    NUM_CLASS = 60
     def __init__(self, root=os.path.expanduser('../datasets'), split='train',
                  mode=None, transform=None, target_transform=None, **kwargs):
         super(ContextSegmentation, self).__init__(
